3cd74bd83785967b4327004a329b26dd1cb36f05
Ruksanasd <ruksanasayyed89@gmail.com>
2024-08-29 10:48:56 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,11 @@
+package com.verizon;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method
+		System.out.println("Welcome to java programming");
+
+	}
+
+}
